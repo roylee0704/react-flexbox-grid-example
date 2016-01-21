9a4300477ed1a0365ba7078e04278d116879d300
roylee0704 <roylee0704@gmail.com>
2016-01-21 14:01:07 +0800

    added react-flexbox-grid example components

---
+++ app/components/box/index.jsx
@@ -0,0 +1,20 @@
+import React, {PropTypes} from 'react';
+import {Col} from 'react-flexbox-grid/lib';
+import box from './style';
+
+const Box = (props) => {
+  return (
+    <Col {...props}>
+        <div className = {box[props.type || 'box']}>
+          {props.children}
+        </div>
+    </Col>
+  );
+};
+
+Box.propTypes = {
+  type: PropTypes.oneOf(['row', 'container', 'nested', 'large']).isRequired,
+  children: PropTypes.node
+};
+
+export default Box;
---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home';
+
+ReactDOM.render(<Home />, document.getElementById('app'));
